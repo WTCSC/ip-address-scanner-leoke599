e7dd0382aee8cd053ad11335b6fce60df69c0556
leoke599 <leoke073@gmail.com>
2025-01-31 10:56:43 -0700

    Added scanning for ip addresses but still need respoinse time

--- scanner.py
+++ scanner.py
@@ -1,9 +1,35 @@
 import ipaddress
 import sys
+import os
+import time
 
+# Takes the cidr from the command line
 cidr = sys.argv[1]
 
-def scan(cidr):
-    ip = ipaddress.ip_network(cidr)
-    for i in ip.hosts():
-        print(i)
+# Verifies your os type
+OS_TYPE = os.name
+
+# Sets the count modifier to the os type
+COUNT = '-n' if OS_TYPE == 'nt' else '-c'
+
+# Empty list to store ip addresses
+ip_list = []
+
+# Creates a network object
+netIpv4Address = ipaddress.ip_network(cidr)
+
+# Iterates through the network object and appends the ip addresses to the list
+for i in netIpv4Address:
+    ip_list.append(str(i))
+
+# Iterates through the list of ip addresses and pings each one
+start = time.time()
+for ip in ip_list:
+    response = os.popen(f"ping {ip} {COUNT} 1").read()
+    start = time.time()
+    end = time.time()
+    elapsed = end - start
+    if "Received = 1" in response and "Approximate" in response:
+        print(f"{ip} - UP ({elapsed:.2f} seconds)")
+    else:
+        print(f"{ip} - DOWN (Connection timeout)")
